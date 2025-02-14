bc3ede907ec0a7831481299bdcdf40068e939b04
aa189 <akash07664@gmail.com>
2025-02-14 22:26:26 +0530

    updated main app file

--- src/main/java/com/grocerymanagementsystem/GrocerymanagementsystemApplication.java
+++ src/main/java/com/grocerymanagementsystem/GrocerymanagementsystemApplication.java
@@ -3,6 +3,7 @@ package com.grocerymanagementsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GrocerymanagementsystemApplication {
 
