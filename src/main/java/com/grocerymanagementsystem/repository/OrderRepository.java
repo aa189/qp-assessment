fdc4d6d480f657b1671aec79b0d97fa4411cf122
aa189 <akash07664@gmail.com>
2025-02-14 23:05:01 +0530

    updated commit

--- src/main/java/com/grocerymanagementsystem/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.grocerymanagementsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.grocerymanagementsystem.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
